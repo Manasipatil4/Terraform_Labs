4a727b6f9fc5340307ff11fc3d33f173805ac3eb
Manasipatil4 <imanasipatilz@gmail.com>
2026-08-10 13:00:58 +0530

    basic file

---
+++ basic.tf
@@ -0,0 +1,14 @@
+# this is a basic terraform file to launch Ec2 instance
+
+provider "aws" {
+    region = "eu-north-1"  
+}
+resource "aws_instance" "ec2" {
+    ami = "ami-0aba19e56f3eaec05"
+    instance_type = "t3.micro"
+    key_name = "manasi"
+
+    tags = {
+      Name = "my_ec2"
+    } 
+}

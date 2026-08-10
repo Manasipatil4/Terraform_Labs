ca55d4362a52eec45a97b4836373a32c4c1cf13e
Manasipatil4 <imanasipatilz@gmail.com>
2026-08-10 15:25:52 +0530

    basic readme and s3

---
+++ s3_basic.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "eu-north-1"  
+}
+resource "aws_s3_bucket" "mys3" {
+    bucket = "manasi-bucket-sample"
+
+    tags = {
+        Name = "My bucket"
+
+    }  
+}
+
+
